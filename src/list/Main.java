8ee3fdfc411ae781e55dff4e9614fff7814603ea
saurabhshcs <saurabhshcs@gmail.com>
2022-11-08 22:30:11 +0000

    Initial LinkedList revision

---
+++ src/list/Main.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class Main {
+  public static void main(String[] args) {
+    LinkedList<String> cars = new LinkedList<String>();
+    cars.add("Mercedese");
+    cars.add("BMW");
+    cars.add("Jaguar");
+    cars.add("Mazda");
+    System.out.println(cars);
+  }
+}
